a6e60b178367a9526fb4a0afe7272d49de07451f
Melanie A <mkkakau@gmail.com>
2016-06-20 18:00:55 -1000

    chores: Add clean and uglify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 var bases = {
   src: 'src/',
   dist: 'dist/'
 };
 
-gulp.task('default', function() {
-  // place code for your default task here
+var paths = {
+  scripts: 'js/*.js'
+
+}
+
+// Clean Task
+// Obliterates dist
+gulp.task('clean', function(){
+  return gulp.src(bases.dist, {read: false})
+    .pipe(clean());
 });
+
+// Uglify Task
+// Uglifies JS
+gulp.task('uglify', function(){
+  return gulp.src(bases.src + paths.scripts)
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(bases.dist + 'js'));
+});
+
+gulp.task('default', ['clean', 'uglify']);
