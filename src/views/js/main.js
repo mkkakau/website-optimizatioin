de12118cf4cd61688644d77d02084cc08fb25929
Melanie A <mkkakau@gmail.com>
2016-06-27 13:22:19 -1000

    fix: Calculate number of pizzas to render based on screen height

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -533,9 +533,12 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
   var s = 256;
-  for (var i = 0; i < 32; i++) {
+  var cols = 8;
+  var rows = Math.floor(window.innerHeight / s);
+  var numberOfBgPizzas = cols * rows;
+
+  for (var i = 0; i < numOfBgPizzas; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
